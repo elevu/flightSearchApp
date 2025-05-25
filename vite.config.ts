f9e56e4c7e11c45f24283c6e3c083ee3fb77e57b
elevu <e.m.container@gmail.com>
2025-05-25 14:15:02 +0200

    Configure Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, // use global test APIs (describe, it, expect)
+    environment: 'jsdom', // or 'happy-dom'
+    setupFiles: ['./vitest.setup.ts'],
+    coverage: {
+      provider: 'istanbul', // or 'c8'
+      reporter: ['text', 'json', 'html'],
+    },
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 })
