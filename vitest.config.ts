00c1621580426dc7113d6f1f0f559056975ac33c
elevu <e.m.container@gmail.com>
2025-05-25 14:16:24 +0200

    Add vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: { alias: { '@': resolve(__dirname, 'src') } },
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: { provider: 'istanbul', reporter: ['text', 'html'] },
+  },
+})
